79d068ac0271f200b9ef3a786fbe79432c932ea3
Oscar Chan <oc.ideas@gmail.com>
2012-04-22 11:59:31 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyTvFileServer::Application.config.secret_token = '47b91c538781d1a4f8333295d0012995df0171836d688053d090f993e847df6810f447100a4eda1cbca95b1bf5adb61c3a4ab0bdea3f0260fd3da4d2a325e223'
